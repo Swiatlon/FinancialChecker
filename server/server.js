cf9fe37970b1835e89b1b26b339bc0895c415915
unknown <wiercikdoc@gmail.com>
2023-03-17 20:16:50 +0100

    Added cors and cookie-parser

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const corsOptions = require('./config/corsOptions');
+
 const app = express();
-const path = require("path");
+
 const PORT = process.env.PORT || 3500;
 
-app.use(express.json())
+app.use(logger);
 
-app.use("/", require("./routes/root"));
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use('/', require('./routes/root'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on PORT:${PORT}`));

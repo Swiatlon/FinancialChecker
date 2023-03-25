13c3b00edc085925d811ad93784d420e124e3d9f
unknown <wiercikdoc@gmail.com>
2023-03-25 21:56:43 +0100

    Initial theme config

---
+++ client/src/assets/theme/theme.js
@@ -0,0 +1,18 @@
+const theme = {
+  colors: {
+    white: '#FFFFFF',
+    lightGrey: '#F7F8FA',
+    grey: '#C0C7D6',
+    darkGrey: '#737C8E',
+    black: '#111111',
+  },
+
+  fontSize: {
+    xl: '24px',
+    l: '20px',
+    m: '16px',
+    s: '14px',
+  },
+};
+
+export default theme;

39bf3175387f502c1361d45815864bb9d6907d60
unknown <wiercikdoc@gmail.com>
2023-03-25 21:56:24 +0100

    RWD

---
+++ client/src/assets/styles/mediaQueries.style.js
@@ -0,0 +1,11 @@
+const size = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '768px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px',
+};
+
+export default size;

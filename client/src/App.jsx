ffc348d810e180eb95b5839181fa21e7ceaf2a11
unknown <wiercikdoc@gmail.com>
2023-03-25 21:55:27 +0100

    Added basic setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,13 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import Layout from './layouts/MainLayout/MainLayout';
+import theme from './assets/theme/theme';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="submit" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Layout />
+    </ThemeProvider>
   );
 }
-
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import GlobalStyle from './assets/styles/GlobalStyle';
+import ErrorPage from './views/ErrorPage/ErrorPage';
 import App from './App';
-import './index.css';
+import PostAuthLayout from './layouts/PostAuthLayout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [{ path: 'postAuth', element: <PostAuthLayout /> }],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <GlobalStyle />
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );

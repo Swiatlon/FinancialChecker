3b05d15991fd8ace24179340fe513a043805d50f
unknown <wiercikdoc@gmail.com>
2023-03-25 21:54:50 +0100

    Added absolute imports

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,13 @@
 /* eslint-disable */
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+       '@': '/src'
+    },
+  },
   plugins: [react()],
 });

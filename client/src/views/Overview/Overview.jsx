93bb5c1f79b83fc6d4d69a1c19a557b2f38cb43e
unknown <wiercikdoc@gmail.com>
2023-03-29 21:50:15 +0200

    Added version 1.0 of overview view

--- client/src/views/Overview/Overview.jsx
+++ client/src/views/Overview/Overview.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Chart as ChartJS } from 'chart.js/auto';
+import { Line } from 'react-chartjs-2';
+import { OverviewContainer, BoxesContainer, SquareBox, RectangleBox, ChartBox, SideDiv } from './Overview.style';
 
 function Overview() {
+  const firstChart = {
+    labels: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
+    datasets: [
+      {
+        data: [0, 4000, 200, 1000, 300, 200, 1500, 3000, 20, 10],
+        borderColor: 'rgb(6, 95, 168)',
+        backgroundColor: 'white',
+      },
+    ],
+  };
+  const secondChart = {
+    labels: [
+      ...Array(31)
+        .fill()
+        .map((_, i) => i + 1),
+    ],
+    datasets: [
+      {
+        data: [0, 4000, 200, 1000, 300, 200, 1500, 3000, 20, 10],
+        borderColor: 'rgb(6, 95, 168)',
+        backgroundColor: 'white',
+      },
+    ],
+  };
+  const FirstOptions = {
+    responsive: true,
+    aspectRadio: 10,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
+  const SecondOptions = {
+    responsive: true,
+    aspectRadio: 10,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
   return (
-    <div>Overview</div>
-  )
+    <OverviewContainer>
+      <SideDiv>
+        <h3>My Wallet:</h3>
+        <BoxesContainer amountOfBoxes="2">
+          <RectangleBox>Amount:&nbsp;0</RectangleBox>
+          <RectangleBox>
+            Monthly Balance: <span className="expenses-red-color">&nbsp;-3423</span>
+          </RectangleBox>
+        </BoxesContainer>
+      </SideDiv>
+      <SideDiv>
+        <h3>This Week:</h3>
+        <ChartBox height="250px">
+          <Line data={firstChart} options={FirstOptions} />
+        </ChartBox>
+      </SideDiv>
+      <SideDiv>
+        <h3>Last expeneses:</h3>
+        <BoxesContainer amountOfBoxes="3">
+          <SquareBox>A</SquareBox>
+          <SquareBox>B</SquareBox>
+          <SquareBox>C</SquareBox>
+          <SquareBox>D</SquareBox>
+          <SquareBox>E</SquareBox>
+          <SquareBox>F</SquareBox>
+        </BoxesContainer>
+      </SideDiv>
+      <SideDiv>
+        <h3>This Month:</h3>
+        <ChartBox height="250px">
+          <Line data={secondChart} options={SecondOptions} />
+        </ChartBox>
+      </SideDiv>
+    </OverviewContainer>
+  );
 }
 
-export default Overview
+export default Overview;
---
+++ client/src/views/Overview/Overview.style.js
@@ -0,0 +1,117 @@
+import styled from 'styled-components';
+import size from '@/assets/styles/mediaQueries.style';
+
+export const a = styled.div``;
+
+export const OverviewContainer = styled.div`
+  width: 90%;
+  min-height: 70%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 40px 40px;
+  color: white;
+  margin-left: 8%;
+  .expenses-red-color {
+    color: red;
+  }
+  :nth-child(1),
+  :nth-child(2) {
+    padding-top: 100px;
+  }
+  @media (max-width: ${size.laptop}) {
+    gap: 40px;
+    margin-left: 0%;
+    width: 100%;
+    grid-template-columns: repeat(1, 1fr);
+    justify-content: center;
+    justify-items: center;
+    text-align: center;
+  }
+`;
+
+export const SideDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  h3 {
+    height: 50px;
+  }
+`;
+
+export const BoxesContainer = styled.div`
+  display: grid;
+  grid-template-columns: ${({ amountOfBoxes }) => `repeat(${amountOfBoxes}, 1fr)`};
+  text-align: center;
+  div {
+    background: #282525;
+    border-left: 1px solid rgba(4, 110, 232, 0.6);
+    border-right: 1px solid rgba(4, 110, 232, 0.6);
+    /* box-shadow: 8px 1px 30px -30px rgb(4 110 232 / 50%); */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  @media (max-width: ${size.tablet}) {
+    justify-content: center;
+    display: flex;
+    flex-wrap: wrap;
+  }
+`;
+
+export const SquareBox = styled.div`
+  max-width: 160px;
+  width: 80%;
+  height: 140px;
+  margin: 20px 0px;
+  @media (max-width: ${size.laptopL}) {
+    height: 120px;
+  }
+  @media (max-width: ${size.laptop}) {
+    width: 100px;
+    height: 100px;
+    margin: 10px 20px;
+  }
+  @media (max-width: ${size.tablet}) {
+    width: 80px;
+    height: 80px;
+    padding: 15px;
+  }
+`;
+
+export const RectangleBox = styled.div`
+  margin-top: auto;
+  margin-bottom: auto;
+  max-width: 230px;
+  width: 80%;
+  height: 100px;
+  padding: 10px;
+  @media (max-width: ${size.laptopL}) {
+    width: 140px;
+    height: 100px;
+  }
+  @media (max-width: ${size.laptop}) {
+    margin: 10px 20px;
+  }
+  @media (max-width: ${size.mobileL}) {
+    width: 150px;
+    height: 60px;
+    font-size: 14px;
+  }
+`;
+
+export const ChartBox = styled.div`
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  margin-top: auto;
+  margin-bottom: auto;
+  @media (max-width: ${size.tablet}) {
+    width: 450px;
+  }
+  @media (max-width: ${size.mobileL}) {
+    width: 300px;
+    height: 150px;
+  }
+  @media (max-width: ${size.mobileM}) {
+    width: 230px;
+    height: 140px;
+  }
+`;

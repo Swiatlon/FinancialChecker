6b0ae1b6d2fe572886252cf3a4e9be59ad2e3bc4
unknown <wiercikdoc@gmail.com>
2023-03-30 18:10:20 +0200

    Added new expenses component

---
+++ client/src/views/NewExpenses/NewExpenses.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { useFieldArray, useForm } from 'react-hook-form';
+import { NewExpensesContainer, ExpensesForm, TwoItemsPerRow, ExpensesItem } from './NewExpenses.style';
+
+function NewExpenses() {
+  const {
+    control,
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({});
+
+  const { fields, append, remove } = useFieldArray({
+    name: 'Items',
+    control,
+  });
+  
+  const addNewItem = () => {
+    console.log(fields);
+    append({
+      name: '',
+      value: '',
+    });
+  };
+
+  const deleteItem = (index) => {
+    remove(index);
+  };
+
+    const onSubmit = (data) => console.log(data);
+
+  return (
+    <NewExpensesContainer>
+      <ExpensesForm onSubmit={handleSubmit(onSubmit)}>
+        <h2>New Expenses</h2>
+        <input {...register('Title', { required: true })} placeholder="Title" />
+
+        <input
+          {...register('Amount', { required: true, min: 0, valueAsNumber: true })}
+          placeholder="Amount"
+          type="number"
+        />
+
+        <input {...register('Location', { required: false })} placeholder="Location" />
+
+        <TwoItemsPerRow>
+          <p>Add Item</p>
+          <input value="+" type="button" onClick={addNewItem} />
+        </TwoItemsPerRow>
+
+        {fields.map((field, index) => {
+          return (
+            <ExpensesItem key={field.id}>
+              <input {...register(`Items.${index}.name`, { required: true })} placeholder="Item" />
+
+              <input
+                {...register(`Items.${index}.value`, { required: true, valueAsNumber: true })}
+                placeholder="Value"
+                type="Number"
+              />
+
+              <input
+                type="button"
+                value="X"
+                onClick={() => {
+                  deleteItem(index);
+                }}
+              />
+            </ExpensesItem>
+          );
+        })}
+        {/* <TwoItemsPerRow>
+          <p>Add Item</p>
+          <input value="+" type="button" />
+        </TwoItemsPerRow>
+        <TwoItemsPerRow>
+          <input type="text" placeholder="Item" />
+          <input type="text" placeholder="Value" />
+        </TwoItemsPerRow> */}
+        <input type="submit" value="Submit" />
+      </ExpensesForm>
+    </NewExpensesContainer>
+  );
+}
+
+export default NewExpenses;

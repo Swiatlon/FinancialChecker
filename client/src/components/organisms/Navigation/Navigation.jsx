5d371e3502b4cac38d0f65c30e40e8e8832c6efb
unknown <wiercikdoc@gmail.com>
2023-04-12 22:55:01 +0200

    Added pre auth menu

--- client/src/components/organisms/Navigation/Navigation.jsx
+++ client/src/components/organisms/Navigation/Navigation.jsx
@@ -8,13 +8,50 @@ import cardIcon from '@/assets/images/icons/credit-card.svg';
 import homeIcon from '@/assets/images/icons/home.svg';
 import expensesIcon from '@/assets/images/icons/trending-down.svg';
 import logoutIcon from '@/assets/images/icons/log-out.svg';
+import registerIcon from '@/assets/images/icons/user-plus.svg';
 
 function Navigation() {
   const [isMenuVisible, setIsMenuVisible] = useState(true);
 
-  const navigationItems = ['Overview', 'My wallet', 'Add new expenses'];
-  const itemsIcons = [homeIcon, cardIcon, expensesIcon];
-  const urls = ['overview', 'wallet', 'newExpenses'];
+  const navigationItemsPreAuth = [
+    { text: 'Home', icon: homeIcon },
+    { text: 'Register', icon: registerIcon },
+    { text: 'Login', icon: logoutIcon },
+  ];
+
+  const navigationItemsAfterAuth = [
+    { text: 'Overview', icon: homeIcon },
+    { text: 'My Wallet', icon: cardIcon },
+    { text: 'Add new expenses', icon: expensesIcon },
+  ];
+
+  const isAuthorized = false;
+
+  const navigationContent = () => {
+    // POST AUTH
+    if (isAuthorized)
+      return (
+        <>
+          {navigationItemsAfterAuth.map((item, index) => (
+            <MenuItem key={`${item + index}`} to={`postAuth/${item.text.replace(/\s/g, '')}`}>
+              <img src={item.icon} alt="menu-icon" />
+              {item.text}
+            </MenuItem>
+          ))}
+          {isAuthorized && <LogoutButton src={logoutIcon} alt="logout-icon" />}
+        </>
+      );
+
+    // PRE AUTH
+    if (!isAuthorized) {
+      return navigationItemsPreAuth.map((item, index) => (
+        <MenuItem key={`${item + index}`} to={`${item.text.replace(/\s/g, '')}`}>
+          <img src={item.icon} alt="menu-icon" />
+          {item.text}
+        </MenuItem>
+      ));
+    }
+  };
 
   const handleClick = () => {
     setIsMenuVisible(!isMenuVisible);
@@ -30,13 +67,7 @@ function Navigation() {
           </LogoContentBox>
           <Hamburger src={HamburgerSrc} alt="menu-logo" onClick={handleClick} />
         </LogoHamburgerContainer>
-        {navigationItems.map((item, index) => (
-          <MenuItem key={`${item + index}`} to={`postAuth/${urls[index]}`}>
-            <img src={itemsIcons[index]} alt="menu-icon" />
-            {item}
-          </MenuItem>
-        ))}
-        <LogoutButton src={logoutIcon} alt="logout-icon" />
+        {navigationContent()}
       </Menu>
     </NavBar>
   );

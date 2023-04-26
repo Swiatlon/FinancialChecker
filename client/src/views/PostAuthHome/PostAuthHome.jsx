f231687afd4eabf562fdbc3f15275094edbf9382
unknown <wiercikdoc@gmail.com>
2023-04-26 23:09:52 +0200

    Created component for first view after auth

---
+++ client/src/views/PostAuthHome/PostAuthHome.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import HomeBox, { ThreeButtonsBox, PostAuthContainer } from '../Home/Home.style';
+// SAME STYLE AS HOME
+
+function PostAuthHome() {
+  return (
+    <PostAuthContainer>
+      <HomeBox>
+        <h2>Welcome</h2>
+        <p>This website will help you track your expenses!</p>
+        <ThreeButtonsBox>
+          <Link to="overview">Get Started!</Link>
+          <Link to="/tutorial">Tutorial</Link>
+        </ThreeButtonsBox>
+      </HomeBox>
+    </PostAuthContainer>
+  );
+}
+
+export default PostAuthHome;

07ba953ef077bfc8411c00660cfc83e2bff3e620
unknown <wiercikdoc@gmail.com>
2023-04-26 23:09:21 +0200

    Deleted unnecessary entity adapter

--- client/src/features/user/userApiSlice.js
+++ client/src/features/user/userApiSlice.js
@@ -1,14 +1,11 @@
-import { createSelector, createEntityAdapter } from '@reduxjs/toolkit';
+import { createSelector } from '@reduxjs/toolkit';
 import apiSlice from '../api/apiSlice';
 
-const userAdapter = createEntityAdapter({});
-
-const initialState = userAdapter.getInitialState();
 
 export const userApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getUser: builder.query({
-      query: (userID) => `api/user?id=${userID}`,  // there will be query which user
+      query: (userID) => `api/user?id=${userID}`, 
       method: 'GET',
       validateStatus: (response, result) => {
         return response.status === 200 && !result.isError;
@@ -17,7 +14,7 @@ export const userApiSlice = apiSlice.injectEndpoints({
       transformResponse: (responseData) => {
         const loadedUser = { ...responseData };
         loadedUser.id = loadedUser._id;
-        return userAdapter.setOne(initialState, loadedUser);
+        return loadedUser;
       },
     }),
 
@@ -28,7 +25,7 @@ export const userApiSlice = apiSlice.injectEndpoints({
         body: {
           ...initialUserData,
         },
-        invalidatesTags: (result, error, arg) => [{ type: 'User', id: 'LIST' }],
+        invalidatesTags: (result, error, arg) => [{ type: 'User', id: 'user' }],
       }),
     }),
 

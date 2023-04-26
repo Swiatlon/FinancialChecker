008f2c1e95cf297a678c9d63643ef2cfaa3ff8ab
unknown <wiercikdoc@gmail.com>
2023-04-26 11:11:31 +0200

    Added auth slice

---
+++ client/src/features/auth/authSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { useSelector } from 'react-redux';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: { token: null },
+  reducers: {
+    setCredentials: (state, action) => {
+      const { accessToken } = action.payload;
+      state.token = accessToken;
+    },
+    logOut: (state, action) => {
+      state.token = null;
+    },
+  },
+});
+
+export const { setCredentials, logOut } = authSlice.actions;
+
+export const selectCurrentToken = (state) => state.auth.token;
+
+export default authSlice.reducer;

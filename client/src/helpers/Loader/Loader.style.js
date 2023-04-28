55a79094ff9cf473f172eed3af597bd5f2bcdd8a
unknown <wiercikdoc@gmail.com>
2023-04-28 19:51:28 +0200

    Loader component

---
+++ client/src/helpers/Loader/Loader.style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const LoaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default LoaderContainer;

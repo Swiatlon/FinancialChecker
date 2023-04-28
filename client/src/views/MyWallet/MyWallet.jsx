cbe3fb92227cb63db844bd5e94e5fc16506011b0
unknown <wiercikdoc@gmail.com>
2023-04-28 20:24:16 +0200

    Initial version of MyWallet component

---
+++ client/src/views/MyWallet/MyWallet.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Loader from '@/helpers/Loader/Loader';
+
+function MyWallet() {
+  return (
+    <>
+      <p style={{ textAlign: 'center' }}>Coming Soon!</p>
+      <Loader />
+    </>
+  );
+}
+
+export default MyWallet;

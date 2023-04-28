366ca4dea20bb719af9348f79274286766b27b26
unknown <wiercikdoc@gmail.com>
2023-04-28 15:24:48 +0200

    Unigue and maximum length of email and username

--- server/models/User.js
+++ server/models/User.js
@@ -7,10 +7,14 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
+      unique: true,
+      max: 50,
     },
 
     name: {
       type: String,
+      unique: true,
+      max: 50,
       default: () => {
         return this.email.split('@')[0]; // Set name as the part before the @ symbol
       },

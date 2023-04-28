3fd9f97f73ea527b014a4086cc514edbb1c08e72
unknown <wiercikdoc@gmail.com>
2023-04-28 19:54:57 +0200

    Refactored code, and added instant login after register

--- client/src/views/Register/Register.jsx
+++ client/src/views/Register/Register.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { setCredentials } from '@/features/auth/authSlice';
 import AuthForm from '@/components/organisms/AuthForm/AuthForm.style';
 import AuthTitle from '@/components/atoms/AuthTitle/AuthTitle.style';
 import AuthTextField from '@/components/molecules/AuthTextField/AuthTextField.style';
 import AuthSubmitButton from '@/components/atoms/AuthButton/AuthButton.style';
 import mailIcon from '@/assets/images/icons/mail.svg';
 import passwordIcon from '@/assets/images/icons/lock.svg';
+import { useLoginMutation, useCreateNewUserMutation } from '@/features/auth/authApiSlice';
+import { alertForSuccessfulAuth, alertForErrors } from '@/helpers/Alerts/Swal';
 
 function Register() {
+  // React Router
+  const navigate = useNavigate();
+
+  // Redux
+  const dispatch = useDispatch();
+  const [registerUser] = useCreateNewUserMutation();
+  const [login] = useLoginMutation();
+
+  // React
   const passwordPattern = /^^(?=.*[\W_])(?=.*[A-Z]).+$/;
   const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  // React Forms
   const {
     register,
     handleSubmit,
@@ -17,12 +33,16 @@ function Register() {
     formState: { errors },
   } = useForm();
 
-  const errorMessageHandler = (error) => {
+  const errorMessageHandler = (error, type) => {
     if (!error) return '';
 
     if (error === 'required') return 'Field is required!';
 
-    if (error === 'pattern') return "Field don't meet pattern!";
+    if (error === 'pattern') {
+      if (type === 'password') return 'Password pattern:\n -one uppercase letter\n -one special char';
+
+      return "Field don't meet pattern!";
+    }
 
     if (error === 'minLength') return 'Too few characters!';
 
@@ -31,9 +51,29 @@ function Register() {
     return "Field don't meet required conditions!";
   };
 
-  const onSubmit = async (data) => {
-    console.log(data);
+  const logUserAfterRegister = async (message, email, password) => {
+    await login({ email, password })
+      .unwrap()
+      .then((result) => {
+        const { accessToken } = result;
+        dispatch(setCredentials({ accessToken }));
+        alertForSuccessfulAuth(message, () => {
+          navigate('/postAuth');
+        });
+      })
+      .catch((err) => alertForErrors(err.data.message));
   };
+
+  const onSubmit = async (data) => {
+    const { email, password } = data;
+    await registerUser({ email, password })
+      .unwrap()
+      .then((result) => {
+        logUserAfterRegister(result.message, email, password);
+      })
+      .catch((err) => alertForErrors(err.data.message));
+  };
+
   return (
     <AuthForm onSubmit={handleSubmit(onSubmit)} hasErrors={errors}>
       <AuthTitle>Register Form</AuthTitle>
@@ -46,7 +86,7 @@ function Register() {
           aria-invalid={errors.email ? 'true' : 'false'}
         />
       </AuthTextField>
-      {errors.email && <span role="alert">{errorMessageHandler(errors.email.type)}</span>}
+      {errors.email && <span role="alert">{errorMessageHandler(errors.email.type, 'email')}</span>}
       <AuthTextField>
         <img src={passwordIcon} alt="password" />
         <input
@@ -57,7 +97,7 @@ function Register() {
           aria-invalid={errors.password ? 'true' : 'false'}
         />
       </AuthTextField>
-      {errors.password && <span role="alert">{errorMessageHandler(errors.password.type)}</span>}
+      {errors.password && <span role="alert">{errorMessageHandler(errors.password.type, 'password')}</span>}
       <AuthSubmitButton type="submit" value="Register" />
     </AuthForm>
   );

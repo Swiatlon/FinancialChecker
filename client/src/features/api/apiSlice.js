b1d7593131c2453d8b88438db9aee19d06d84597
unknown <wiercikdoc@gmail.com>
2023-04-29 12:21:02 +0200

    Cors deploy problems

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setCredentials } from '../auth/authSlice';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://financial-checker-api.onrender.comm',
+  baseUrl: 'https://financial-checker-api.onrender.com',
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const { token } = getState().auth;
